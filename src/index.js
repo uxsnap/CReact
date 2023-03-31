9637de463742b52eb74b0bac88a4d9f81bedf565
nuxxxx <nuxxx7132@gmail.com>
2023-03-31 18:53:55 +0700

    1_working_prototype Add refs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { Todo } from './examples/Todo';
+import { CustomTextInput } from './examples/RefTest';
 import { render, createElement } from './main/render';
 import './style/index.scss';
 
 /** @jsxRuntime classic */
 /** @jsx createElement */
 
-render(<Todo />, document.getElementById('app'));
+// render(<Todo />, document.getElementById('app'));
+render(<CustomTextInput />, document.getElementById('app'));
