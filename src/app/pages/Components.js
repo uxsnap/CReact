89a9a654d389226092c5944905e027ca036a0443
nuxxxx <nuxxx7132@gmail.com>
2023-04-13 18:45:23 +0700

    2_tutorial Update Components

--- src/app/pages/Components.js
+++ src/app/pages/Components.js
@@ -15,66 +15,68 @@ export const Components = () => (
     <Title>Handling components</Title>
 
     <Paragraph>
-      In this chapter we will learn how to handle components in our apps.
+      In this chapter, we will learn how to handle components in the <CodeWrap>JSX</CodeWrap>.
     </Paragraph>
 
     <Paragraph>
-      The components can be instances of<CodeWrap>Function</CodeWrap> and <CodeWrap>Class</CodeWrap>.
-      <CodeWrap>JSX</CodeWrap> parser parses both types as a functions so firstly we need to handle 
-      functions in <CodeWrap>render</CodeWrap> function.
+      The components can be instances of<CodeWrap>Function</CodeWrap>or<CodeWrap>Class</CodeWrap>.
+      <CodeWrap>JSX</CodeWrap> parser can handle both types as functions. Firstly,
+      we need to handle functions in a<CodeWrap>render</CodeWrap> function.
     </Paragraph>
 
     <CodeBlock code={RENDER_3} fileName="main/render.js" line="21-25,27-34" />
 
     <List items={[
-      <Paragraph>We create separate function to handle components - <CodeWrap>renderComponent</CodeWrap>.</Paragraph>,
-      <Paragraph>On lines <CodeWrap>27-34</CodeWrap> we're handling <CodeWrap>Fragments</CodeWrap>. 
+      <Paragraph>On the lines <CodeWrap>21-25</CodeWrap>, we call separate function to handle components - <CodeWrap>renderComponent</CodeWrap>.</Paragraph>,
+      <Paragraph>On the lines<CodeWrap>27-34</CodeWrap>, we're handling<CodeWrap>Fragments</CodeWrap>. 
         The <CodeWrap>Fragment</CodeWrap> doesn't have attributes so we need to skip <CodeWrap>setProp</CodeWrap>
         if the element is fragment.
       </Paragraph>
     ]} />
 
     <Paragraph>
-      In function <CodeWrap>renderComponent</CodeWrap> we check if the component is an
+      In function<CodeWrap>renderComponent</CodeWrap>, we check if the component is an
       instance of <CodeWrap>Component</CodeWrap> class - the class that will handle stateful components.
-      Otherwise, we call function with the props as it's functional component. Also, there is specific prop of the components
-      <CodeWrap>children</CodeWrap> - it can be added through adding <CodeWrap>vdom.children</CodeWrap> to the <CodeWrap>props</CodeWrap> 
+      If it's not, we call function with the props as a functional component. Also, there is a specific prop of the components -
+      <CodeWrap>children</CodeWrap>. It can be added through adding<CodeWrap>vdom.children</CodeWrap>to the <CodeWrap>props</CodeWrap> 
       of the component. 
     </Paragraph>
 
     <CodeBlock code={RENDER_COMPONENT_1} fileName="main/render.js" line="2,4-10,12-15"/>
 
     <List items={[
-      <Paragraph>On line <CodeWrap>2</CodeWrap> we add <CodeWrap>children</CodeWrap> to the other props</Paragraph>,
-      <Paragraph>On lines <CodeWrap>4-10</CodeWrap> we create instance of the class component 
+      <Paragraph>On the line <CodeWrap>2</CodeWrap>, we add <CodeWrap>children</CodeWrap> to the other props</Paragraph>,
+      <Paragraph>On the lines <CodeWrap>4-10</CodeWrap>, we create instance of the class component 
         and add specific fields to the instance. It's very expensive to recreate class components all the time
-        so this fields will help us <CodeWrap>reconcile</CodeWrap> class components without recreation of the instance.</Paragraph>,
-      <Paragraph>On lines <CodeWrap>12-15</CodeWrap> we create functional components. It's simple - we just need to pass 
-      props as the arguments of the function</Paragraph>
+        so this fields will help us<CodeWrap>reconcile</CodeWrap>class components without recreation of the instance.</Paragraph>,
+      <Paragraph>On the lines <CodeWrap>12-15</CodeWrap>, we create functional components. It's simple - we just need to pass 
+      props as the arguments of the function.</Paragraph>
     ]}/>
 
-    <Paragraph>Also, let's not forget adding specific functional component - <CodeWrap>Fragment</CodeWrap></Paragraph>
+    <Paragraph>Also, let's not forget to add specific functional component - <CodeWrap>Fragment</CodeWrap>.</Paragraph>
 
     <CodeBlock code={FRAGMENT_1} fileName="main/render.js"/>
   
     <Paragraph>Then we need to update <CodeWrap>reconcile</CodeWrap> function to handle components.
-    We also create helper function - <CodeWrap>reconcileComponent</CodeWrap>, to handle reconciliation.</Paragraph>
+    We also create helper function - <CodeWrap>reconcileComponent</CodeWrap> - to handle reconciliation of the components.</Paragraph>
     
     <CodeBlock code={RECONCILE_4} fileName="main/reconcile.js" line="4-6"/>
     <CodeBlock code={RECONCILE_5} fileName="main/reconcile.js" />
     <CodeBlock code={RECONCILE_6} fileName="main/reconcile.js" />
 
-    <Paragraph>Here we check if the instances of the <CodeWrap>dom</CodeWrap> and <CodeWrap>vdom</CodeWrap> are the same.
-      If they're not, we need to render a new one. If the instances are the same, we have to reconcile old one with the new props
+    <Paragraph>Here we check if the instances of the<CodeWrap>dom</CodeWrap>and<CodeWrap>vdom</CodeWrap>are the same.
+      If they're not, we need to render a new one. If the instances are the same, we have to reconcile old one with the new props.
     </Paragraph>
 
     <Paragraph>And finally, we need to add <CodeWrap>Component</CodeWrap> class. The code of the file is pretty simple.
-      We only need to assign <CodeWrap>props</CodeWrap> to coming props and <CodeWrap>__dom</CodeWrap> to <CodeWrap>null</CodeWrap>. 
+      We only need to assign <CodeWrap>props</CodeWrap> to the coming props and <CodeWrap>__dom</CodeWrap> to <CodeWrap>null</CodeWrap>. 
     </Paragraph>
 
     <CodeBlock code={COMPONENT_1} fileName="main/component.js" />
 
     <Paragraph>With all that, we can now update our tests of <CodeWrap>render</CodeWrap> and <CodeWrap>reconciliation</CodeWrap>.</Paragraph>
+
+    <Paragraph>Codesandbox:</Paragraph>
 
     <SandboxFrame n={3} />
   </Fragment>
