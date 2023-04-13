dcb2634bbc43f73fecb6c051cdf68cb21c29c39d
nuxxxx <nuxxx7132@gmail.com>
2023-04-13 22:55:07 +0700

    2_tutorial Update State

--- src/app/pages/State.js
+++ src/app/pages/State.js
@@ -16,117 +16,124 @@ export const State = () => (
     <Title>Adding state</Title>
 
     <Paragraph>
-      In this chapter we will add <CodeWrap>state</CodeWrap> to our components and 
-      <CodeWrap>lifecycle hooks</CodeWrap> so we can finally write yourself a <CodeWrap>Todo</CodeWrap>!
+      In this chapter, we will add<CodeWrap>state</CodeWrap> and 
+      <CodeWrap>lifecycle hooks</CodeWrap>to our components, 
+      so we can finally write a<CodeWrap>Todo</CodeWrap>!
     </Paragraph>
 
 
-    <Paragraph>The state of the component will be handled in the <CodeWrap>Component</CodeWrap> class.
+    <Paragraph>
+      The<CodeWrap>state</CodeWrap>of the component will be handled in the<CodeWrap>Component</CodeWrap>class.
     </Paragraph>
 
     <CodeBlock code={COMPONENT_2} fileName="main/component.js" line="8-10,13-21,23-28"/>
 
     <List items={[
-      <Paragraph>On the lines <CodeWrap>8-10</CodeWrap>, 
-      we added the <CodeWrap>state</CodeWrap> field and the special field <CodeWrap>__CALL_QUEUE</CodeWrap>. 
-      This field is created to contain all the state changes that are fired within the component.</Paragraph>,
       <Paragraph>
-        On the lines <CodeWrap>23-28</CodeWrap>, 
-        we are adding new state changes to the queue. The <CodeWrap>requestIdleCallback</CodeWrap> will fire its callback when the main thread is idle.
+        On the lines <CodeWrap>8-10</CodeWrap>, 
+        we add the<CodeWrap>state</CodeWrap>field and the special field <CodeWrap>__CALL_QUEUE</CodeWrap>. 
+        The<CodeWrap>__CALL_QUEUE</CodeWrap>field's main purpose is 
+        to contain all the<CodeWrap>state</CodeWrap>changes 
+        that are fired within the component.
+      </Paragraph>,
+      <Paragraph>
+        On the lines<CodeWrap>23-28</CodeWrap>, 
+        we add new<CodeWrap>state</CodeWrap>changes to the queue. The <CodeWrap>requestIdleCallback</CodeWrap> will fire its callback when the main thread is idle.
       </Paragraph>,
       <Paragraph>
         On the lines <CodeWrap>13-21</CodeWrap>, 
-        our state will be updated with all the changes that were contained in the 
+        the <CodeWrap>state</CodeWrap>will be updated with all the changes that were contained in the 
         <CodeWrap>__CALL_QUEUE</CodeWrap>. Following that, 
-        we will reconcile the component and reassign necessary special fields."
+        we will reconcile the component and reassign necessary special fields.
       </Paragraph>
     ]} />
 
     <Paragraph>
-      Now the class components are stateful. Inside the <CodeWrap>class</CodeWrap>
-      lifecycle methods were created. Most of them do nothing as they should. We only need to specify
-      <CodeWrap>return</CodeWrap> value of <CodeWrap>shouldComponentUpdate</CodeWrap> and 
-      <CodeWrap>getSnapshotBeforeUpdate</CodeWrap> as React documentation insists. 
+      Now the class components are stateful. Inside the<CodeWrap>class</CodeWrap>
+      lifecycle methods were added. Most of them do nothing as they should. We only need to specify
+      <CodeWrap>return</CodeWrap>value of<CodeWrap>shouldComponentUpdate</CodeWrap>and 
+      <CodeWrap>getSnapshotBeforeUpdate</CodeWrap>as<CodeWrap>React</CodeWrap>documentation insists. 
     </Paragraph>
 
 
     <Paragraph>
-      Let's also create <CodeWrap>PureComponent</CodeWrap>. The main difference between classic component and
-      pure one is in <CodeWrap>shouldComponentUpdate</CodeWrap>. <CodeWrap>PureComponent</CodeWrap>
-      makes shallow comparison of the old and new props; old and new state, and updates if the objects are not the same 
+      Let's also create<CodeWrap>PureComponent</CodeWrap>. The main difference between a classic component and
+      a pure one is in the<CodeWrap>shouldComponentUpdate</CodeWrap> method.
+      A <CodeWrap>PureComponent</CodeWrap>makes shallow comparison 
+      of the old and new props, old and new state, and reconciles if the objects are not the same.
     </Paragraph>
 
     <CodeBlock code={HELPER_2} fileName="helpers/utils.js" />
     <CodeBlock code={PURE_COMPONENT_1} fileName="main/pureComponent.js" />
 
-    <Paragraph>The last thing we have to do, is to add lifecycle methods calls to <CodeWrap>render</CodeWrap>
-    and <CodeWrap>reconcile</CodeWrap> functions.</Paragraph>
+    <Paragraph>The last thing we have to do is to add lifecycle method calls to the<CodeWrap>render</CodeWrap>
+    and the<CodeWrap>reconcile</CodeWrap>functions.</Paragraph>
 
     <CodeBlock code={RENDER_COMPONENT_2} fileName="main/render.js" line="7,12"/>
 
     <List items={[
       <Paragraph>
-        On the line <CodeWrap>7</CodeWrap> we call <CodeWrap>getDerivedStateFromProps</CodeWrap> to 
-        create <CodeWrap>state</CodeWrap> through <CodeWrap>props</CodeWrap> object.
+        On the line<CodeWrap>7</CodeWrap>, we call<CodeWrap>getDerivedStateFromProps</CodeWrap>to 
+        create<CodeWrap>state</CodeWrap>through the<CodeWrap>props</CodeWrap>object.
       </Paragraph>,
       <Paragraph>
-      On the line <CodeWrap>12</CodeWrap> we call <CodeWrap>componentDidMount</CodeWrap> as 
-      our freshly created component has been mounted to the <CodeWrap>DOM</CodeWrap>
-    </Paragraph>,
+        On the line<CodeWrap>12</CodeWrap>, we call<CodeWrap>componentDidMount</CodeWrap>after 
+        our freshly created component has been mounted to the<CodeWrap>DOM</CodeWrap>.
+      </Paragraph>,
     ]}/>
 
-    <Paragraph>The code for the <CodeWrap>getDerivedStateFromProps</CodeWrap>:</Paragraph>
+    <Paragraph>The code for the<CodeWrap>getDerivedStateFromProps</CodeWrap>:</Paragraph>
 
     <CodeBlock code={GET_DERIVED_STATE_1} fileName="helpers/lifecycle.js" />
 
     <Paragraph>
-      In <CodeWrap>reconcile</CodeWrap>, before destroying components
-      we need to call <CodeWrap>componentWillUnmount</CodeWrap>:
+      In<CodeWrap>reconcile</CodeWrap>
+      we need to call<CodeWrap>componentWillUnmount</CodeWrap>before destroying components:
     </Paragraph>
 
     <CodeBlock code={LIFECYCLE_1} fileName="main/reconcile.js" />
 
-
     <Paragraph>
-      In <CodeWrap>reconcileClassComponent</CodeWrap>:
+      In<CodeWrap>reconcileClassComponent</CodeWrap>function:
     </Paragraph>
 
     <CodeBlock code={RECONCILE_8} fileName="main/reconcile.js" line="6,11,13-15,19"/>
 
     <List items={[
       <Paragraph>
-        On the line <CodeWrap>6</CodeWrap> we call <CodeWrap>getDerivedStateFromProps</CodeWrap>.
+        On the line<CodeWrap>6</CodeWrap>, we call<CodeWrap>getDerivedStateFromProps</CodeWrap>.
       </Paragraph>,
       <Paragraph>
-        On the line <CodeWrap>11</CodeWrap> we call <CodeWrap>getSnapshotBeforeUpdate</CodeWrap>. 
-        Method is returning <CodeWrap>null</CodeWrap> or <CodeWrap>snapshot</CodeWrap> 
+        On the line<CodeWrap>11</CodeWrap>, we call<CodeWrap>getSnapshotBeforeUpdate</CodeWrap>. 
+        Method returns<CodeWrap>null</CodeWrap>or<CodeWrap>snapshot</CodeWrap>
         of the previous props and state.
       </Paragraph>,
       <Paragraph>
-        On the lines <CodeWrap>13-15</CodeWrap> we call <CodeWrap>shouldComponentUpdate</CodeWrap>. 
-        Method is returning <CodeWrap>boolean</CodeWrap> value and is used mostly to optimize rerendering.
+        On the lines<CodeWrap>13-15</CodeWrap>, we call<CodeWrap>shouldComponentUpdate</CodeWrap>. 
+        Method returns<CodeWrap>boolean</CodeWrap>value and is used mostly to optimize rerendering.
       </Paragraph>,
       <Paragraph>
-        On the line <CodeWrap>19</CodeWrap> we call <CodeWrap>componentDidUpdate</CodeWrap>. 
-        If the method <CodeWrap>shouldComponentUpdate</CodeWrap> returns <CodeWrap>true</CodeWrap>,
-        the component will call <CodeWrap>componentDidUpdate</CodeWrap> method after the reconciliation.
+        On the line<CodeWrap>19</CodeWrap>, we call<CodeWrap>componentDidUpdate</CodeWrap>. 
+        If the method<CodeWrap>shouldComponentUpdate</CodeWrap>returns <CodeWrap>true</CodeWrap>,
+        the component will call the<CodeWrap>componentDidUpdate</CodeWrap>method after the reconciliation.
       </Paragraph>,
     ]}/>
 
     <Paragraph>
-      Also, we need to update <CodeWrap>setProp</CodeWrap> so the <CodeWrap>input</CodeWrap> components can handle <CodeWrap>value</CodeWrap>
-      properly.
+      Also, we need to update<CodeWrap>setProp</CodeWrap>function so the<CodeWrap>input</CodeWrap>components can handle <CodeWrap>value</CodeWrap>
+      prop properly.
     </Paragraph>
 
     <CodeBlock code={SET_PROP_2} fileName="main/render.js" />
 
-    <Paragraph>With all the changes, the <CodeWrap>Todo</CodeWrap> can finally be created.</Paragraph>
+    <Paragraph>With all the changes, the<CodeWrap>Todo</CodeWrap>can finally be created.</Paragraph>
 
     <SandboxFrame n={4} />
 
     <Paragraph>
       P.S. There are still props that our components can't handle right now and some bugs left to fix.
-      We'll do it in the next chapter. <CodeWrap>onChange</CodeWrap> is working unexpectedly (it's working when input loses focus) but the desired behavior 
+      We'll do it in the next chapter.<CodeWrap>onChange</CodeWrap>is working unexpectedly (it's working when<CodeWrap>input</CodeWrap>loses focus) 
+      but the desired behavior 
       will be implemented in the next chapter.
     </Paragraph>
   </Fragment>
